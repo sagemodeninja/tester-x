abfde81f5c13307c80f3c334bae58ca33b7e2e76
Gary Antier <contact@garyantier.com>
2026-02-11 20:01:46 +0800

    feat: basic

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('Hello, World!');
+class View {
+    _input1;
+    _addBtn;
+    _result;
+
+    constructor() {
+        this._input1 = document.getElementById("input1");
+        this._addBtn = document.getElementById("add-btn");
+        this._result = document.getElementById("result");
+
+        this._addBtn.addEventListener("click", () => {
+            const num1 = parseFloat(this._input1.value);
+            const result = this.add(num1);
+            this._result.textContent = `Result: ${result}`;
+        });
+    }
+
+    add(num1) {
+        return num1 + 10;
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const view = new View();
+});
